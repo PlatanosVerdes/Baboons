8c9477d25910f2d845297eb53dd563dce01cd455
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2021-12-28 17:05:47 +0100

    Added comments

--- src/main.adb
+++ src/main.adb
@@ -1,19 +1,21 @@
--- with Ada.Integer.Text_IO; use Ada.Integer.Text_IO;
+-- Autores: Jorge González Pascual & Rubén Palmer Pérez
+-- Video: 
+
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Numerics.Discrete_Random;
 with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
-with def_monitor;
-use def_monitor;
+with def_monitor; use def_monitor;
 
--- Compilar: gnatmake main.adb | .\main.exe
+-- Compilar sin GNAT STUDIO: gnatmake main.adb | .\main.exe
 
 procedure Main is
-   THREADS : constant integer := 10;                        -- Numero de babuinos "total"
-   MAX_PASS : constant integer := 3;                        -- Numero de veces que un babuino pasa por la cuerda
-   MIN_RANDOM: constant integer := 1;                       -- Minimo valor aleatorio
-   MAX_RANDOM: constant integer := 5;                       -- Maximo valor aleatorio 
-   NORT: constant string := ESC & "[1;34m" & "NORTE" & ESC & "[0;37m";   -- Nort format string
-   SUR : constant string := ESC & "[1;36m" & "SUR" & ESC & "[0;37m" ;    -- Sur format string
+   NORTE_BABOONS : constant integer := 5;                               -- Numero de babuinos del NORTE
+   SUR_BABOONS : constant integer := 5;                                 -- Numero de babuinos del SUR                                    
+   MAX_PASS : constant integer := 3;                                    -- Numero de veces que un babuino pasa por la cuerda
+   MIN_RANDOM: constant integer := 1;                                   -- Minimo valor aleatorio
+   MAX_RANDOM: constant integer := 5;                                   -- Maximo valor aleatorio 
+   NORT: constant string := ESC & "[1;34m" & "NORTE" & ESC & "[0;37m";  -- Nort format string
+   SUR : constant string := ESC & "[1;36m" & "SUR" & ESC & "[0;37m" ;   -- Sur format string
 
    -- Random number generator
    -- Use: Random(G)
@@ -27,8 +29,7 @@ procedure Main is
    -- Tipo protegido para las tareas
    cuerda: BaboonsMonitor; 
 
-   -- Init tasks
-
+   -- INIT TASKS
    -- Init task Norte 
    task type babuinoNorte is
       entry Start(idx : in integer);
@@ -51,25 +52,21 @@ procedure Main is
       
       -- Pasadas de un babuino por la cuerda  
       for pass in 1..MAX_PASS loop
-         -- Pasar por la cuerda 
          cuerda.baboonNthLock;
-            
-         -- Cruzar
-         delay(Duration(velocidad));
-         -- delay(Duration(Random(G)));
+         
+         delay(Duration(velocidad));  -- Cruzar la cuerda 
 
          cuerda.baboonNthUnLock;
          Put_Line(NORT & My_Idx'Img & " he llegado");
             
-         -- Volver a la zona correspondiente - delay
-         delay(Duration(velocidad));
+         delay(Duration(velocidad));  -- Volver a subir
+
          if (pass = MAX_PASS)then
             Put_Line(NORT&My_Idx'img&": da la vuelta y acaba!!");
          else
             Put_Line(NORT&My_Idx'img&": vuelta"& pass'Img &" /"&MAX_PASS'Img);
          end if;
       end loop; 
-
    end babuinoNorte;
 
    -- Init task Sur
@@ -95,18 +92,14 @@ procedure Main is
       
       -- Pasadas de un babuino por la cuerda  
       for pass in 1..MAX_PASS loop
-         -- Pasar por la cuerda 
          cuerda.baboonSthLock;
 
-         -- Cruzar
-         delay(Duration(velocidad));
-         -- delay(Duration(Random(G)));
+         delay(Duration(velocidad));  -- Cruzar la cuerda 
 
          cuerda.baboonSthUnLock;
          Put_Line(SUR & My_Idx'Img & " he llegado");
       
-         -- Volver a la zona correspondiente - delay
-         delay(Duration(velocidad));
+         delay(Duration(velocidad)); -- Volver a subir
 
          if (pass = MAX_PASS)then
             Put_Line(SUR&My_Idx'img&": da la vuelta y acaba!!");
@@ -114,19 +107,24 @@ procedure Main is
             Put_Line(SUR&My_Idx'img&": vuelta"& pass'Img &" /"&MAX_PASS'Img);
          end if;
       end loop;
-      
    end babuinoSur;
 
    -- Array de babuinos del Norte
-   baboonsNorte : array (1..THREADS/2) of babuinoNorte;
+   baboonsNorte : array (1..NORTE_BABOONS) of babuinoNorte;
 
    -- Array de babuinos del Sur
-   baboonsSur : array (1..THREADS/2) of babuinoSur;
+   baboonsSur : array (1..SUR_BABOONS) of babuinoSur;
 
 begin
    Reset(G); -- Reset de la seed de los valores aleatorios 
-   for Idx in 1..THREADS/2 loop
+
+   -- Inicializar los babuinos del Norte
+   for Idx in 1..NORTE_BABOONS loop
       baboonsNorte(Idx).Start(Idx);
+   end loop;
+
+   -- Inicializar los babuinos del Sur 
+   for Idx in 1..SUR_BABOONS loop
       baboonsSur(Idx).Start(Idx);
    end loop;
 
